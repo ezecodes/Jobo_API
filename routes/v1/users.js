6531428beb2be490f27c125562a9b8f5f12cad5d
jahdevelops <mail>
2023-10-28 08:54:55 -0700

    update user routes handler functions

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -1,19 +1,66 @@
 var express = require('express');
 var router = express.Router();
 
+const {
+  validateUser,
+  validateUserCredentials
+} = require('./middlewares/user')
+
+const {
+  generatePasswordHash
+} = require('../../utils/user')
+
+const {
+  NODE_ENV
+} = require('../../config')
+
+const userService = require('../../services/user')
+
 /* GET users listing. */
-router.get('/:user_id',  function(req, res, next) {
+router.get('/:user_id', validateUser, function(req, res, next) {
   
 });
 
 
 /* POST users listing */
 router.post('/', async (req, res) => {
-  
+  const {email, password} = req.body
+
+  if (!email) {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid Email Value'
+    })
+  }
+  if (!password) {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid Password Value'
+    })
+  }
+
+  try {
+    const hashedPassword = await generatePasswordHash(password)
+    const newUser = await UserModel.create({email, password: hashedPassword})
+
+    return res.status(201).json({
+      success: true,
+      message: 'User Created Successfully',
+      data: {email}
+    })
+  } catch (err) {
+    logger.debug(err)
+  }
 })
-router.post('/login', async (req, res) => {
-  const {username, password} = req.body
-  
+
+router.post('/login', validateUserCredentials, async (req, res) => {
+  res.cookie('token', token, {
+    signed: true,
+    path: '/user',
+    maxAge: 1000 * 60 * 60, 
+    httpOnly: true, 
+    secure: NODE_ENV === 'production' ? true : false
+  })
 })
 
 
