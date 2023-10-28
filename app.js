eaf35d3d7c5b92bdedde7388508e0744d98f9c73
jahdevelops <mail>
2023-10-28 03:02:10 -0700

    define routes

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouterV1 = require('./routes/v1/users');
+var budgetRouterV1 = require('./routes/v1/budget');
 
 var app = express();
 
@@ -15,6 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(require('helmet')())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -26,8 +27,13 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.disable("etag");
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
+
+app.use('/api/v1/users', usersRouterV1);
+app.use('/api/v1/budget', budgetRouterV1);
+app.use('/api/v1/health', (req, res) => res.sendStatus(200));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
