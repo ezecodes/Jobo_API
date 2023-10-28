51bae6f96be33f09410f82ce71b3602dfed8a5cc
jahdevelops <mail>
2023-10-28 04:18:38 -0700

    define config vars

--- config/index.js
+++ config/index.js
@@ -1,27 +1,12 @@
 require('dotenv').config()
-
 const mongoose = require('mongoose');
 const db = mongoose.connection
-require('dotenv').config()
-
-db.once('open', () => console.log(`DB connected`));
-console.log('db connecting...');
-
-(async function establishConnection() {
-    try {
-        await mongoose.connect( process.env.DB_URL , {
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-        });
-    } catch (err) {
-        console.log('DB failed to connect')
-        console.error(err)
-    }
-})();
-
 
 module.exports = {
 	db,
+	NODE_ENV: process.env.NODE_ENV,
+    CLIENT_ID: process.env.clientId,
+    SECRET_KEY: process.env.secretKey,
 	OpenBanking_primary_key: process.env.FCMB_open_banking_primary_key,
 	OpenBanking_secondary_key: process.env.FCMB_open_banking_secondary_key,
 }
