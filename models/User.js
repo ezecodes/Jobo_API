9df8e476a1e23c566d804e945d5be202a0d7c0b9
jahdevelops <mail>
2023-10-28 12:33:41 -0700

    remove require attribute from user model

--- models/User.js
+++ models/User.js
@@ -7,18 +7,17 @@ const userSchema = new mongoose.Schema({
 	first_name: {
 		type: String,
 		lowercase: true,
-		required: true
 	},
 	last_name: {
 		type: String,
 		lowercase: true,
-		required: true
 	},
 	email: {
 		type: String,
 		required: true,
 		lowercase: true
 	},
+	pin: {type: String, lowercase: true},
 	budgets: [{type: mongoose.Types.ObjectId, ref: 'Budget'}]
 })
 
