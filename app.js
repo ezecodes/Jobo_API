121dda29c010fa9e360fb64d7676fddd4aa34043
jahdevelops <mail>
2023-10-30 03:39:50 -0700

    add jwt secret for cookie-parser module

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var morganLog = require('morgan');
 const logger = require('./logger')
+const {USER_COOKIE_SECRET} = require('./config')
 
 var usersRouterV1 = require('./routes/v1/users');
 var budgetRouterV1 = require('./routes/v1/budgets');
@@ -14,16 +15,14 @@ app.use(morganLog('dev'));
 app.use(require('helmet')())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser(USER_COOKIE_SECRET));
 
 app.disable("etag");
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 app.use('/api/v1/users', usersRouterV1);
-app.use('/api/v1/budget', budgetRouterV1);
+app.use('/api/v1/budgets', budgetRouterV1);
 app.use('/api/v1/health', (req, res) => res.sendStatus(200));
 
 // catch 404 and forward to error handler
