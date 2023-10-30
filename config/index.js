06d3982d606dc875896722523e74852cc49f1302
jahdevelops <mail>
2023-10-30 03:42:34 -0700

    add config constants

--- config/index.js
+++ config/index.js
@@ -6,15 +6,12 @@ module.exports = {
 	db,
 	DB_URL: process.env.DB_URL,
 	NODE_ENV: process.env.NODE_ENV,
-    CLIENT_ID: process.env.CLIENT_ID,
-    SECRET_KEY: process.env.SECRET_KEY,
-	OPEN_BANKING_PK: process.env.OPEN_BANKING_PK,
-	OPEN_BANKING_SK: process.env.OPEN_BANKING_SK,
 
 	USER_JWT_SECRET: process.env.USER_JWT_SECRET,
-	USER_COOKIE_SECRET: process.env.USER_JWT_SECRET,
+	USER_COOKIE_SECRET: process.env.USER_COOKIE_SECRET,
 
 	CONSTANTS: {
 		BUDGET_PRIORITY_LEVELS: ['high', 'medium', 'low'],
+		BUDGET_STATUSES: ['pending', 'done', 'failed']
 	}
 }
