1b25f20dc65128edfcac0a2a336f52c65f179bda
jahdevelops <mail>
2023-10-30 03:43:01 -0700

    add new fields

--- models/Budget.js
+++ models/Budget.js
@@ -2,8 +2,6 @@
 const mongoose = require('mongoose')
 const {CONSTANTS} = require('../config')
 
-const User = require('./User')
-
 const budgetSchema = new mongoose.Schema({
 	title: {
 		type: String,
@@ -15,16 +13,26 @@ const budgetSchema = new mongoose.Schema({
 		required: true,
 		lowercase: true
 	},
+	status: {
+		type: String,
+		default: CONSTANTS.BUDGET_STATUSES[0],
+		enum: {values: CONSTANTS.BUDGET_STATUSES}
+	},
 	amount: {
 		type: Number,
 		required: true
 	},
+	label: {type: String},
 	priority_level: {
 		type: String,
 		enum:{values: CONSTANTS.BUDGET_PRIORITY_LEVELS}
 	},
+	recipient_account_name: {type: String},
+	recipient_bank: {type: String},
+	recipient_account_number: {type: Number},
 
-	created_by: {type: mongoose.Types.ObjectId, ref: 'User'}
-})
+	created_by: {type: mongoose.Types.ObjectId, ref: 'User'},
+	
+}, {timestamps: true})
 
 module.exports = mongoose.model('Budget', budgetSchema)
