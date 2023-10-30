99287e51b725634293d522767e936e7c22aa3f4b
jahdevelops <mail>
2023-10-30 03:48:31 -0700

    delete unused constants

--- utils/user.js
+++ utils/user.js
@@ -2,7 +2,7 @@ const crypto = require('crypto')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-const { CLIENT_ID, SECRET_KEY, USER_JWT_SECRET } = require('../config')
+const {USER_JWT_SECRET } = require('../config')
 
 async function generatePasswordHash(password) {
   const salt_round = 10
@@ -18,26 +18,7 @@ async function validatePassword(password, hashedPassword) {
   return await bcrypt.compare(password, hashedPassword)
 }
 
-function generateSHA512String() {
-  const now = new Date();
-  
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, '0'); // Month is zero-based
-  const day = String(now.getDate()).padStart(2, '0');
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const seconds = String(now.getSeconds()).padStart(2, '0');
-  const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
-
-  const formattedDateTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
-  
-	let data = crypto.createHash('sha512').update(`${formattedDateTime}${CLIENT_ID}${SECRET_KEY}`)
-
-	return data.digest('hex')
-}
-
 module.exports = {
-	generateSHA512String, 
   generatePasswordHash,
   createJwtToken,
   validatePassword
